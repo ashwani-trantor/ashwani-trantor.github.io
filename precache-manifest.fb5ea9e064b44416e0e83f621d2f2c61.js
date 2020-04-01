33ae5ce419e03aa568d04cb8e3a573f1c051dda1
Ashwani Goyal <ashwani.goyal@trantorinc.com>
2020-04-01 12:43:19 +0530

    First Production Build

---
+++ precache-manifest.fb5ea9e064b44416e0e83f621d2f2c61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc735d30e3394e22bd355bbd3837cc04",
+    "url": "/index.html"
+  },
+  {
+    "revision": "217370648bf28b95cca7",
+    "url": "/static/css/main.47367b20.chunk.css"
+  },
+  {
+    "revision": "68ac4dcedf9b91af97ce",
+    "url": "/static/js/2.4628c337.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.4628c337.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "217370648bf28b95cca7",
+    "url": "/static/js/main.9cda8b5a.chunk.js"
+  },
+  {
+    "revision": "81786c6fd50766777295",
+    "url": "/static/js/runtime-main.780b3817.js"
+  }
+]);
